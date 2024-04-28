6b7cf3b96d13a06cec352080a147071dad7d725d
Paul Bullock <paul.bullock@capacreative.co.uk>
2024-04-28 15:51:48 +0100

    reduce build file for test site

--- docfx/ghbuild.ps1
+++ docfx/ghbuild.ps1
@@ -3,10 +3,6 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
-./main/docfx/report-matrix.ps1 -BaseDir "./main" -ReportFile "./main/docfx/matrix.md"
-./main/docfx/report-metadata.ps1 -BaseDir "./main" -ReportFile "./main/docfx/metadata.md"
-./main/docfx/report-cmdusage.ps1 -BaseDir "./main" -ReportFile "./main/docfx/cmdusage.md"
-
 docfx build ./main/docfx/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
