4893286b39ec2cf8aa3d180656c4f114386494b5
Paul Bullock <paul.bullock@capacreative.co.uk>
2024-06-02 13:10:40 +0100

    sample.json combined

---
+++ docfx/samples.ps1
@@ -0,0 +1,23 @@
+# Get all sample.json files in scripts/<sample>/assets folders
+$jsonFiles = Get-ChildItem -Path ..\scripts -Filter sample.json -Recurse | Where-Object { $_.DirectoryName -like "*\assets" }
+
+# Initialize an empty array to hold the content of all json files
+$jsonContent = @()
+
+# Loop through each json file
+foreach ($file in $jsonFiles) {
+    # Get the content of the file
+    $content = Get-Content $file.FullName | ConvertFrom-Json -Depth 6
+
+    # Here the information can be reduced to the necessary information only to reduce file size e.g.,
+    # name, title, url, creationDateTime, updateDateTime, shortDescription,products,categories, tags, metadata, thumbnails, authors
+
+    # Add the content to the array
+    $jsonContent += $content
+}
+
+# Convert the array to json
+$combinedJson = $jsonContent | ConvertTo-Json -Depth 6
+
+# Write the combined json to a file
+$combinedJson | Out-File -FilePath .\samples.json
