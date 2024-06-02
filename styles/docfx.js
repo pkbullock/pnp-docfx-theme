cda9764281d69f0b606d14683a7760fb196a1062
Paul Bullock <paul.bullock@capacreative.co.uk>
2024-06-02 12:35:29 +0000

    Commit from GitHub Actions (Generate Site)

--- styles/docfx.js
+++ styles/docfx.js
@@ -1212,5 +1212,309 @@ $(function () {
 
   }
 
+  //-----------------------------------------------------------------------------------
+  // Card Browser
+  //-----------------------------------------------------------------------------------
+
+  /**
+ * This file is unique for each sample browser. It contains the logic specific to each repo for loading the samples as needed.
+ * https://pkbullock.github.io/pnp-docfx-theme/
+ */
+  var baseAddress = (document.location.host === "pkbullock.github.io") ? document.location.origin + "/pnp-docfx-theme" : document.location.origin;
+  var jsonPath = baseAddress + "/samples.json";
+  
+  /**
+   * Reads a sample metadata and returns a pre-populated HTML element
+   * @param {*} sample 
+   * @returns 
+   */
+  function loadSample(sample, filter) {
+    try {
+      // _ is missing
+      // var title = _.escape(sample.title);
+      // var escapedDescription = _.escape(sample.shortDescription);
+
+      var title = sample.title;
+      var escapedDescription = sample.shortDescription;
+
+      var shortDescription = sample.shortDescription; //.length > 80 ? sample.shortDescription.substr(0, 77)  : sample.shortDescription;
+      var thumbnail = baseAddress + "/assets/nopreview.png";
+
+
+      if (sample.thumbnails && sample.thumbnails.length > 0) {
+        thumbnail = sample.thumbnails[0].url;
+      }
+
+      var libraries = "";
+      var operations = "";
+      var products = "";
+
+
+      var metadata = sample.metadata;
+      metadata.forEach(meta => {
+        if (libraries !== "") {
+          libraries = libraries + ", ";
+        }
+        libraries = libraries + meta.key.toLowerCase();
+      });
+
+      // Operations are represented as catagories in the sample.json file
+      sample.categories.forEach(cata => {
+        if (operations !== "") {
+          operations = operations + ", ";
+        }
+        operations = operations + cata.toLowerCase();
+      });
+
+      // Products
+      sample.products.forEach(product => {
+        if (products !== "") {
+          products = products + ", ";
+        }
+        products = products + product.toLowerCase();
+      });
+
+      var modified = new Date(sample.updateDateTime).toString().substr(4).substr(0, 12);
+      var authors = sample.authors;
+      var authorsList = "";
+      var authorAvatars = "";
+      var authorName = "";
+      var authorsGitHub = "";
+      var productTag = sample.products[0].toLowerCase();
+      var productName = sample.products[0];
+      //var library = metadata[0].key.toLowerCase();
+
+      var tools = "";
+      var toolCount = 0;
+      metadata.forEach(library => {
+        var toolToUse = library.key.toLowerCase();
+        tools = tools + `<div class="producttype-item ${toolToUse} tool-${toolCount}">${toolToUse}</div>`;
+        toolCount++;
+      });
+
+      // Build the authors array
+      if (authors.length < 1) {
+        console.log("Sample has no authors", sample);
+      } else {
+        authors.forEach(author => {
+          if (authorsList !== "") {
+            authorsList = authorsList + ", ";
+          }
+          authorsList = authorsList + author.name;
+          authorsGitHub = authorsGitHub + " " + author.gitHubAccount;
+
+          var authorAvatar = `<div class="author-avatar">
+              <div role="presentation" class="author-coin">
+                <div role="presentation" class="author-imagearea">
+                  <div class="image-400">
+                    <img class="author-image" loading="lazy" src="${author.pictureUrl}" alt="${author.name}" title="${author.name}">
+                  </div>
+                </div>
+              </div>
+            </div>`;
+          authorAvatars = authorAvatar + authorAvatars;
+        });
+
+        authorName = authors[0].name;
+        if (authors.length > 1) {
+          authorName = authorName + ` +${authors.length - 1}`;
+        }
+      }
+
+      // Extract tags
+      var tags = "";
+      $.each(sample.tags, function (_u, tag) {
+        tags = tags + "#" + tag + ",";
+      });
+
+      // Build a keyword tag for searching
+      var keywords = title + " " + escapedDescription + " " + authorsList + " " + authorsGitHub + " " + tags;
+      keywords = keywords.toLowerCase();
+
+      // Build the HTML to insert
+      var $items = $(`
+<a class="sample-thumbnail" href="${sample.url}" data-modified="${sample.updateDateTime}" data-title="${title}" data-keywords="${keywords}" data-tags="${tags}" data-libraries="${libraries}" data-operation="${operations}" data-products="${products}">
+  <div class="sample-inner">
+    <div class="sample-preview">
+      <img src="${thumbnail}" loading="lazy" alt="${title}">
+    </div>
+    <div class="sample-details">
+      ${tools}
+      <div class="sample-text">
+        <p class="sample-title" title="${sample.title}">${sample.title}</p>
+        <p class="sample-description" title='${escapedDescription}'>${shortDescription}</p>
+      </div>
+      <div class="sample-activity">
+        ${authorAvatars}
+        <div class="activity-details">
+          <span class="sample-author" title="${authorsList}">${authorName}</span>
+          <span class="sample-date">Modified ${modified}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</a>`);
+
+      return $items;
+    } catch (error) {
+      console.log("Error with one sample", error, sample);
+    }
+    return null;
+  }
+
+  // external js: isotope.pkgd.js
+
+  /***
+   * This is a generic isotope filter for samples
+   */
+
+  $(document).ready(function () {
+    var filterText = $('#sample-listing').data("filter");
+    var qsRegex;
+    var buttonFilter;
+    // var urlParams = new URLSearchParams(window.location.search);
+    // var query = urlParams.get('query');
+
+    // init Isotope
+    var $grid = $('#sample-listing').isotope({
+      itemSelector: '.sample-thumbnail',
+      layoutMode: 'fitRows',
+      sortBy: 'modified',
+      sortAscending: false,
+      getSortData: {
+        modified: '[data-modified]',
+        title: '.sample-title'
+      },
+      filter: function () {
+
+        //var urlResult = query ? $(this).data("keywords").match(query): true;
+        var searchResult = qsRegex ? $(this).data("keywords").match(qsRegex) : true;
+        var buttonResult = buttonFilter ? $(this).is(buttonFilter) : true;
+
+        return searchResult && buttonResult;
+      },
+
+      fitRows: {
+        columnWidth: '.grid-sizer'
+      }
+
+    });
+
+    // Display/hide a message when there are no results
+    $grid.on('arrangeComplete', function (_event, filteredItems) {
+      if (filteredItems.length > 0) {
+        // hide message 
+        $("#noresults").hide();
+      } else {
+        // show message; 
+        $("#noresults").show();
+      }
+    });
+
+    // Get the JSON
+    $.getJSON(jsonPath, function (data) {
+      var asc = true;
+      var prop = "updateDateTime";
+
+      // Sort data descending order
+      data = data.sort(function (a, b) {
+        try {
+          if (asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+          else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+        } catch (error) {
+          return 0;
+        }
+      });
+
+      $.each(data, function (_u, sample) {
+
+        var item = loadSample(sample, filterText);
+
+        if (item !== null) {
+          $grid.append(item)
+            // add and lay out newly appended items
+            .isotope('appended', item);
+        }
+
+      });
+
+      // Update the sort
+      $grid.isotope('updateSortData').isotope();
+    });
+
+    // Get the list of filters to use
+    var filter = $('#filters .filter-choice');
+
+    // Get the search box
+    var search = $('#post-search-input');
+
+    $('.filter-list').each(function (_i, buttonGroup) {
+      var $buttonGroup = $(buttonGroup);
+      $buttonGroup.on('click', '.filter-choice', function () {
+        $buttonGroup.find('.active').removeClass('active');
+        $(this).addClass('active');
+        var filters = [];
+
+        filter.filter('.active').each(function () {
+          filters.push($(this).data("filter"));
+        });
+
+        filters = filters.join('');
+        buttonFilter = filters;
+        $grid.isotope();
+
+      });
+    });
+
+    search.on('change keyup paste', debounce(function () {
+      qsRegex = new RegExp(search.val(), 'gi');
+      $grid.isotope();
+
+      // Update the URL
+      var url = window.location.href;
+      var urlParts = url.split("?");
+      var searchVal = search.val();
+      var newUrl = urlParts[0];
+
+      if (searchVal.length > 0) {
+        var newUrl = urlParts[0] + "?query=" + searchVal;
+      }
+
+      window.history.pushState({}, "", newUrl);
+
+    }, 200));
+
+    // debounce so filtering doesn't happen every millisecond
+    function debounce(fn, threshold) {
+      var timeout;
+      return function debounced() {
+        if (timeout) {
+          clearTimeout(timeout);
+        }
+        function delayed() {
+          fn();
+          timeout = null;
+        }
+        timeout = setTimeout(delayed, threshold || 100);
+      };
+    }
+
+    // See if there are any passed parameters
+    try {
+
+      // Do not search immediately as grid isn't fully loaded at this point
+      $grid.one('arrangeComplete', function () {
+        var urlParams = new URLSearchParams(window.location.search);
+        var query = urlParams.get('query');
+        if (query !== "") {
+          search.val(query).change();
+        }
+      });
+
+    } catch (error) {
+      // Be vewy vewy quiet
+    }
+  });
+
 
 });
